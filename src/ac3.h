6f96adbd8ab7342c0d85a1ccc3eb21668aad0f24
Eugen Sawin <sawine@me73.com>
2012-10-24 16:53:46 +0200

    Added some comments.

--- src/ac3.h
+++ src/ac3.h
@@ -11,20 +11,38 @@ namespace ace {
 class Network;
 class Assignment;
 
+// AC3 (Arc Consistency 3) Preprocessor. Also used for MAP iterations.
 class Ac3 : public Preprocessor {
  public:
+  // Initializes preprocessor with given network.
   explicit Ac3(Network* network);
 
+  // Propagates arc-consistency for given variable id.
   bool Propagate(const int var_id);
+  
+  // Makes network fully arc-consistent.
   bool Preprocess();
+
+  // Resets temporary data stored between propagations.
   void Reset();
 
+  // Returns the duration in microseconds of the last call to propagate or
+  // preprocess.
   base::Clock::Diff duration() const;
+
+  // Returns the number of iterations used for the last call to propagate or
+  // preprocess.
   int num_iterations() const;
+  
+  // Returns the number over domain value reductions in the last call to
+  // propagate or preprocess.
   int num_processed() const;
 
  private:
+  // Intermediate structure for revise operations.
   struct ReviseItem {
+    // Initializes revice item with given constraint id and variable scope
+    // index.
     ReviseItem(const int constraint, const int var_index)
         : constraint(constraint),
           var_index(var_index) {}
@@ -32,6 +50,7 @@ class Ac3 : public Preprocessor {
     int var_index;
   };
 
+  // Queue type used for domain change propagation.
   typedef std::queue<ReviseItem> Queue;
 
   bool Enforce(Queue* queue);
--- src/variable.h
+++ src/variable.h
@@ -8,24 +8,52 @@
 
 namespace ace {
 
+// Representation of a constraint variable.
 class Variable {
  public:
+  // Initialized the variable with given name, reference domain id and valid
+  // domain values.
   Variable(const std::string& name, const int domain_id,
            const std::set<int>& domain);
 
+  // Starts a transaction. All domain modifications are recorded and reversable
+  // during the transaction.
   void StartTransaction();
+  
+  // Commits the transaction making the domain modifications of the current
+  // transactions persistent and irreversable.
+  // Closes the active transaction.
   void CommitTransaction();
+
+  // Rolls back all domain modifications of the currently active transaction.
+  // Closes the active transaction.
   void RollbackTransaction();
+ 
+  // Reduces the valid domain value ids to given value id.
   void ReduceDomain(const int value_id);
+
+  // Removes the given value id from the valid domain value ids. 
   void RemoveValue(const int value_id);
-  // void SwapDomain(std::vector<int>* values);
-  // void domain(const std::vector<int>& values);
+
+  // Returns the value for given value id.
   int value(const int value_id) const;
+
+  // Returns the number of valid values.
   int num_values() const;
+
+  // Returns a copy of the valid domain values.
   std::vector<int> domain() const;
+
+  // Returns a copy of the valid domain value ids.
   std::vector<int> valid_value_ids() const;
+
+  // Returns whether the given value is valid in the domain (by id).
   bool valid(const int value_id) const;
+
+  // Returns the name of the variable.
   std::string name() const;
+
+  // Returns the reference domain id of the variable.
   int domain_id() const;
 
  private:
